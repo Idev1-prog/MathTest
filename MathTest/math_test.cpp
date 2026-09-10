aad9f681865f3ee49e2a20ba8ff11947d78047c0
Ivan <Kornillovivan707@mail.ru>
2026-09-10 14:10:25 +0300

    Минимально добавлена структура, так же начато описание класса MathTest

--- MathTest/math_test.cpp
+++ MathTest/math_test.cpp
@@ -1,1 +1,17 @@
 #include "math_test.h"
+
+int generator(int min, int max) {
+	return min + rand() % (max - min + 1);
+}
+
+MathTest::MathTest() {
+	task.num1 = rand() % 200;
+	task.num2 = rand() % 200;
+	task.oper = '+';
+	task.answer = task.num1 + task.num2;
+}
+MathTest::MathTest(int min, int max, char op) {
+	task.oper = op;
+	task.num1 = generator(min, max);
+	task.num2 = generator(min, max);
+}
--- MathTest/math_test.h
+++ MathTest/math_test.h
@@ -1,13 +1,19 @@
 #pragma once
+#include <random>
 
 struct  Task
 {
-
+	int num1;
+	int num2;
+	char oper;
+	int answer;
 };
 
 class MathTest
 {
+	Task task;
 public:
 	MathTest();
-	~MathTest();
+	MathTest(int min, int max, char op);
+	~MathTest() = default;
 };
