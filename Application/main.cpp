9200a244093b2f163a7feab16d3ff871499f4e9e
Ivan <Kornillovivan707@mail.ru>
2026-09-10 13:42:48 +0300

    Добавлен шаблон проекта

---
+++ Application/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#define GREETING "Привет!!!"
+
+int main() {
+	std::cout << GREETING;
+	return 200;
+}
---
+++ MathTest/math_test.h
@@ -0,0 +1,13 @@
+#pragma once
+
+struct  Task
+{
+
+};
+
+class MathTest
+{
+public:
+	MathTest();
+	~MathTest();
+};
